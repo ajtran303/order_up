67163c1c952f09423fd3976a11da31cf9a856c0e
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-24 09:54:29 -0600

    Add method test .total_calories to Dish model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,20 @@ RSpec.describe Dish, type: :model do
     it { should have_many :dish_ingredients }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+  describe "methods" do
+    before :each do
+      @chef_too_sweet = Chef.create!(name: "Too Sweet")
+
+      @sugar_water = @chef_too_sweet.dishes.create!(name: "Sugar Water", description: "It's too sweet!")
+
+      @water = Ingredient.create!(name: "Water", quantity: 1, units: "L", calories_per_unit: 0)
+      @sugar = Ingredient.create!(name: "Sugar", quantity: 3, units: "cups", calories_per_unit: 700)
+
+      @sugar_water.ingredients << [@sugar, @water]
+    end
+
+    it ".total_calories" do
+      expect(@sugar_water.total_calories).to eq(2100)
+    end
+  end
 end
